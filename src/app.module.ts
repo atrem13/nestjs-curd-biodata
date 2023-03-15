a31e1589e3417fc58393e85abf0368304e68f339
atrem13 <mertayoga17.my@gmail.com>
2023-03-15 16:24:44 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { BiodataModule } from './biodata/biodata.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, BiodataModule],
   controllers: [AppController],
   providers: [AppService],
 })
